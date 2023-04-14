ee002c5c72cd9d7148de84bbf0cfbface6406635
MOHAMED RILWAN S <mohamed.rilwan14@gmail.com>
2023-04-14 13:08:42 +0100

    Updates

---
+++ precache-manifest.f206e6f49992aece2b2eec25d633ca19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "851e4a88577aa45edc38",
+    "url": "/static/css/main.1ac8243f.chunk.css"
+  },
+  {
+    "revision": "851e4a88577aa45edc38",
+    "url": "/static/js/main.851e4a88.chunk.js"
+  },
+  {
+    "revision": "ff3e70574e23ec862a62",
+    "url": "/static/js/1.ff3e7057.chunk.js"
+  },
+  {
+    "revision": "d0da21c5f1c8f20199f8",
+    "url": "/static/js/2.d0da21c5.chunk.js"
+  },
+  {
+    "revision": "452ecbd6c2de88b254e0",
+    "url": "/static/js/runtime~main.452ecbd6.js"
+  },
+  {
+    "revision": "be6228998e31904c0d42d148597680fa",
+    "url": "/static/media/logo.be622899.svg"
+  },
+  {
+    "revision": "c3fc645d919e345549498e386c751f2e",
+    "url": "/index.html"
+  }
+];
